adba94443ffd33180e8818fc7da2c4b4c24d3b12
Victor Alexandre Padilha <victorpadilha.cc@gmail.com>
2017-03-14 17:22:16 -0300

    added io module docs

--- biclustlib/io.py
+++ biclustlib/io.py
@@ -2,14 +2,33 @@ import json
 
 from models import Bicluster, Biclustering
 
-def save_biclusterings(b, file_path, extension='.json'):
-    with open(file_path + extension, 'w') as f:
+def save_biclusterings(b, file_path, extension='json'):
+    """Dumps biclusterings to a file using the json module.
+
+    Parameters
+    ----------
+    b : biclustlib.models.Biclustering or list
+        A Biclustering instance or a list of Biclustering instances that will be saved.
+
+    file_path : str
+        The path of the file where the Biclustering instance or list of Biclustering instances will be saved.
+
+    extension : str, default: 'json'
+        The file extension to be used.
+    """
+    with open(file_path + '.' + extension, 'w') as f:
         json.dump(b, f, default=_biclustering_to_dict)
 
-def load_biclusterings(filename):
-    with open(filename, 'r') as f:
-        biclusterings = json.load(f, object_hook=_dict_to_biclustering)
+def load_biclusterings(file_path):
+    """Load biclusterings from a json file.
 
+    Parameters
+    ----------
+    file_path : str
+        The path of the file where the Biclustering instance or list of Biclustering instances are stored.
+    """
+    with open(file_path, 'r') as f:
+        biclusterings = json.load(f, object_hook=_dict_to_biclustering)
     return biclusterings
 
 def _biclustering_to_dict(bic):

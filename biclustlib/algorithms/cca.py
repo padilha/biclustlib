0a083aca29befa782a32e9140424ddaba0ca4e97
Victor Alexandre Padilha <victorpadilha.cc@gmail.com>
2018-03-28 18:43:16 -0300

    added option to estimate the MSR threshold

--- biclustlib/algorithms/cca.py
+++ biclustlib/algorithms/cca.py
@@ -41,8 +41,10 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
     num_biclusters : int, default: 10
         Number of biclusters to be found.
 
-    msr_threshold : float, default: 0.1
+    msr_threshold : float or str, default: 'estimate'
         Maximum mean squared residue accepted (delta parameter in the original paper).
+        If 'estimate', the algorithm will calculate this threshold as:
+        (((max(data) - min(data)) ** 2) / 12) * 0.005.
 
     multiple_node_deletion_threshold : float, default: 1.2
         Scaling factor to remove multiple rows or columns (alpha parameter in the original paper).
@@ -51,7 +53,7 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
         Minimum number of dataset columns required to perform multiple column deletion.
     """
 
-    def __init__(self, num_biclusters=10, msr_threshold=0.1, multiple_node_deletion_threshold=1.2, data_min_cols=100):
+    def __init__(self, num_biclusters=10, msr_threshold='estimate', multiple_node_deletion_threshold=1.2, data_min_cols=100):
         self.num_biclusters = num_biclusters
         self.msr_threshold = msr_threshold
         self.multiple_node_deletion_threshold = multiple_node_deletion_threshold
@@ -73,18 +75,20 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
         min_value = np.min(data)
         max_value = np.max(data)
 
+        msr_thr = (((max_value - min_value) ** 2) / 12) * 0.005 if self.msr_threshold == 'estimate' else self.msr_threshold
+
         biclusters = []
 
         for i in range(self.num_biclusters):
             rows = np.ones(num_rows, dtype=np.bool)
             cols = np.ones(num_cols, dtype=np.bool)
 
-            self._multiple_node_deletion(data, rows, cols)
-            self._single_node_deletion(data, rows, cols)
+            self._multiple_node_deletion(data, rows, cols, msr_thr)
+            self._single_node_deletion(data, rows, cols, msr_thr)
             self._node_addition(data, rows, cols)
 
-            row_indices = np.where(rows)[0]
-            col_indices = np.where(cols)[0]
+            row_indices = np.nonzero(rows)[0]
+            col_indices = np.nonzero(cols)[0]
 
             if len(row_indices) == 0 or len(col_indices) == 0:
                 break
@@ -98,19 +102,19 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
 
         return Biclustering(biclusters)
 
-    def _single_node_deletion(self, data, rows, cols):
+    def _single_node_deletion(self, data, rows, cols, msr_thr):
         """Performs the single row/column deletion step (this is a direct implementation of the Algorithm 1 described in
         the original paper)"""
         msr, row_msr, col_msr = self._calculate_msr(data, rows, cols)
 
-        while msr > self.msr_threshold:
+        while msr > msr_thr:
             self._single_deletion(data, rows, cols, row_msr, col_msr)
             msr, row_msr, col_msr = self._calculate_msr(data, rows, cols)
 
     def _single_deletion(self, data, rows, cols, row_msr, col_msr):
         """Deletes a row or column from the bicluster being computed."""
-        row_indices = np.where(rows)[0]
-        col_indices = np.where(cols)[0]
+        row_indices = np.nonzero(rows)[0]
+        col_indices = np.nonzero(cols)[0]
 
         row_max_msr = np.argmax(row_msr)
         col_max_msr = np.argmax(col_msr)
@@ -122,27 +126,24 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
             col2remove = col_indices[col_max_msr]
             cols[col2remove] = False
 
-    def _multiple_node_deletion(self, data, rows, cols):
+    def _multiple_node_deletion(self, data, rows, cols, msr_thr):
         """Performs the multiple row/column deletion step (this is a direct implementation of the Algorithm 2 described in
         the original paper)"""
         msr, row_msr, col_msr = self._calculate_msr(data, rows, cols)
 
-        if msr <= self.msr_threshold:
-            stop = True
-        else:
-            stop = False
+        stop = True if msr <= msr_thr else False
 
         while not stop:
             cols_old = np.copy(cols)
             rows_old = np.copy(rows)
 
-            row_indices = np.where(rows)[0]
+            row_indices = np.nonzero(rows)[0]
             rows2remove = row_indices[np.where(row_msr > self.multiple_node_deletion_threshold * msr)]
             rows[rows2remove] = False
 
             if len(cols) >= self.data_min_cols:
                 msr, row_msr, col_msr = self._calculate_msr(data, rows, cols)
-                col_indices = np.where(cols)[0]
+                col_indices = np.nonzero(cols)[0]
                 cols2remove = col_indices[np.where(col_msr > self.multiple_node_deletion_threshold * msr)]
                 cols[cols2remove] = False
 
@@ -151,7 +152,7 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
             # Tests if the new MSR value is smaller than the acceptable MSR threshold.
             # Tests if no rows and no columns were removed during this iteration.
             # If one of the conditions is true the loop must stop, otherwise it will become an infinite loop.
-            if msr <= self.msr_threshold or (np.all(rows == rows_old) and np.all(cols == cols_old)):
+            if msr <= msr_thr or (np.all(rows == rows_old) and np.all(cols == cols_old)):
                 stop = True
 
     def _node_addition(self, data, rows, cols):
@@ -232,8 +233,8 @@ class ChengChurchAlgorithm(BaseBiclusteringAlgorithm):
         if self.num_biclusters <= 0:
             raise ValueError("num_biclusters must be > 0, got {}".format(self.num_biclusters))
 
-        if self.msr_threshold < 0.0:
-            raise ValueError("msr_threshold must be >= 0.0, got {}".format(self.msr_threshold))
+        if self.msr_threshold != 'estimate' and self.msr_threshold < 0.0:
+            raise ValueError("msr_threshold must be equal to 'estimate' or a numeric value >= 0.0, got {}".format(self.msr_threshold))
 
         if self.multiple_node_deletion_threshold < 1.0:
             raise ValueError("multiple_node_deletion_threshold must be >= 1.0, got {}".format(self.multiple_node_deletion_threshold))
